65243a01d2bcc67a773ba6e9016720306ac02aac
Tom Mertz & virginie trubiano <student@epicodus.com>
2015-03-09 15:47:57 -0700

    Done ping pong

--- app/app.php
+++ app/app.php
@@ -1,7 +1,7 @@
 <?php
 
     require_once __DIR__."/../vendor/autoload.php";
-    require_once __DIR__."/../src/TitleCaseGenerator.php";
+    require_once __DIR__."/../src/PingPongGenerator.php";
 
 
     $app = new Silex\Application();
@@ -13,10 +13,10 @@
         return $app['twig']->render('form.twig');
     });
 
-    $app->get("/view_title_case", function() use ($app)  {
-        $my_TitleCaseGenerator = new TitleCaseGenerator;
-        $title_cased_phrase = $my_TitleCaseGenerator->makeTitleCase($_GET['phrase']);
-        return $app['twig']->render('title_cased.twig', array('result' => $title_cased_phrase));
+    $app->get("/view_result", function() use ($app)  {
+        $my_PingPongGenerator = new PingPongGenerator;
+        $ping_pong_array = $my_PingPongGenerator->generatePingPongArray($_GET['number']);
+        return $app['twig']->render('ping_pong.twig', array('results' => $ping_pong_array));
     });
 
     return $app;
--- src/PingPongGenerator.php
+++ src/PingPongGenerator.php
@@ -1,27 +1,27 @@
 <?php
     class PingPongGenerator
     {
-            function generatePingPongArray($input_number)
-            {
-                $ping_pong_array = array();
+        function generatePingPongArray($input_number)
+        {
+            $ping_pong_array = array();
 
-                for ($counter = 1; $counter <= $input_number; $counter++) {
-                    if ($counter % 3 == 0 && $counter % 5 == 0) {
-                        array_push($ping_pong_array, "ping pong");
-                    }
-                    elseif ($counter % 3 == 0) {
-                        array_push($ping_pong_array, "ping");
-                    }
-                    elseif ($counter % 5 == 0) {
-                        array_push($ping_pong_array, "pong");
-                    }
-                    else {
-                        array_push($ping_pong_array, $counter);
-                    }
+            for ($counter = 1; $counter <= $input_number; $counter++) {
+                if ($counter % 3 == 0 && $counter % 5 == 0) {
+                    array_push($ping_pong_array, "ping pong");
                 }
-
-                return $ping_pong_array;
+                elseif ($counter % 3 == 0) {
+                    array_push($ping_pong_array, "ping");
+                }
+                elseif ($counter % 5 == 0) {
+                    array_push($ping_pong_array, "pong");
+                }
+                else {
+                    array_push($ping_pong_array, $counter);
+                }
             }
+
+            return $ping_pong_array;
+        }
     }
 
 ?>
