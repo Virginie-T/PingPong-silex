7bea47199cc64f83886275ccc441f5604e3db671
Tom Mertz & virginie trubiano <student@epicodus.com>
2015-03-09 15:21:01 -0700

    add ping pong

--- src/PingPongGenerator.php
+++ src/PingPongGenerator.php
@@ -4,9 +4,16 @@
             function generatePingPongArray($input_number)
             {
                 $ping_pong_array = array();
+
                 for ($counter = 1; $counter <= $input_number; $counter++) {
-                    if ($counter % 3 == 0) {
+                    if ($counter % 3 == 0 && $counter % 5 == 0) {
+                        array_push($ping_pong_array, "ping pong");
+                    }
+                    elseif ($counter % 3 == 0) {
                         array_push($ping_pong_array, "ping");
+                    }
+                    elseif ($counter % 5 == 0) {
+                        array_push($ping_pong_array, "pong");
                     }
                     else {
                         array_push($ping_pong_array, $counter);
--- tests/PingPongGeneratorTest.php
+++ tests/PingPongGeneratorTest.php
@@ -60,6 +60,44 @@
             //assert
             $this->assertEquals([1,2,'ping'], $result);
         }
+
+        /*
+        input: 5
+        output: [1,2,”ping”, 4, “pong”]
+
+        spec: Count from 1 to the number input, replacing numbers divisible by 5 with pong
+        */
+
+        function test_makePingPong_pong()
+        {
+            //arrange
+            $test_PingPongGenerator = new PingPongGenerator;
+            $input = 5;
+
+            //act
+            $result = $test_PingPongGenerator->generatePingPongArray($input);
+
+            //assert
+            $this->assertEquals([1,2,'ping',4,'pong'], $result);
+        }
+
+        /*
+        input: 15
+        output: [1, 2 , ‘ping’, 4, ‘pong’, …, 14, ‘ping pong’]
+        spec: count form 1to the number input, replacing numbers divisible by 3 and 5 with ping pong
+        */
+        function test_makePingPong_pingPong()
+        {
+            //arrange
+            $test_PingPongGenerator = new PingPongGenerator;
+            $input = 15;
+
+            //act
+            $result = $test_PingPongGenerator->generatePingPongArray($input);
+
+            //assert
+            $this->assertEquals([1,2,'ping',4,'pong','ping',7,8,'ping','pong',11,'ping',13,14,'ping pong'], $result);
+        }
     }
 
 
